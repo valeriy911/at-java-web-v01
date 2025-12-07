3ac40ec584321ece33716e15212137e56647cce5
Sergey Slesarev <slqamsk@gmail.com>
2025-12-07 10:11:09 +0300

    add

---
+++ src/test/java/demo/part02/NavigationTests.java
@@ -0,0 +1,20 @@
+package demo.part02;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+public class NavigationTests {
+    @Test
+    void test_navigation() {
+        open("https://ya.ru/");
+        sleep(10_000);
+        open("https://ru.wikipedia.org");
+        sleep(3_000);
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        forward();
+        sleep(10_000);
+    }
+}
