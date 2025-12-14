c54e3a2ddb5b7cff38ac18250f0676c836f434f8
Valeriy Kozlov <Valeriy_911@mail.ru>
2025-12-14 11:06:38 +0300

    Day3_2

--- src/test/java/work/part03/testxPathAddPizza.java
+++ src/test/java/work/part03/testxPathAddPizza.java
@@ -1,7 +1,7 @@
 package work.part03;
 
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
@@ -19,4 +19,25 @@ public class testxPathAddPizza {
         SelenideElement se3 = $x("//h3[.='Маргарита']/../button");
         System.out.println("Кнопка: тег: " + se3.getTagName() + ", текст: " + se3.text());
     }
+
+    @Test
+    void testHWspecialist() {
+        open("https://www.specialist.ru/");
+        sleep(2_000);
+        $x("//button[@id='cookieConsent__ok']").click();
+        sleep(2_000);
+        $x("//a[.='Курсы']").click();
+        sleep(2_000);
+        $x("//a[.='Каталог курсов']").click();
+        sleep(10_000);
+
+        //$("#CourseName").sendKeys("тестирование");
+        $x("//input[@id='CourseName']").sendKeys("тестирование");
+        $x("//button[@type='submit']").click();
+
+        String myXPath = "//*[contains(text(),'Автоматизированное тестирование веб-приложений с использованием Selenium')]/ancestor::article//dd[contains(@class,'date-start') and contains(@class,'date')]";
+        SelenideElement se = $x(myXPath);
+        se.shouldHave(text("24.01.2026"));
+    }
+
 }
